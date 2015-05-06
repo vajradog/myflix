db409f30266d3244d614624aa4b0f67c1a223fb5
vajradog <vajradog@gmail.com>
2015-05-06 09:36:52 -0400

    created the video show page, action and route

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,30 +9,30 @@
 Video.create(
 	title: "Spirited Away", 
 	description: "In this animated feature by noted Japanese director Hayao Miyazaki, 10-year-old Chihiro (Rumi Hiiragi) and her parents (Takashi Naitô, Yasuko Sawaguchi) stumble upon a seemingly abandoned amusement park. After her mother and father are turned into giant pigs.",
-	small_cover_url: "spirited_away.jpg",
-	large_cover_url: "/spirited_away_large.jpg" )
+	small_cover_url: "/tmp/spirited_away.jpg",
+	large_cover_url: "/tmp/spirited_away_large.jpg" )
 
 
 Video.create(
 	title: "My Neighbor Totoro", 
 	description: "Two sisters encounter a mythical forest sprite and its woodland companions when they move to rural Japan.",
-	small_cover_url: "totoro.jpg",
-	large_cover_url: "totoro_large.jpg" )
+	small_cover_url: "/tmp/totoro.jpg",
+	large_cover_url: "/tmp/totoro_large.jpg" )
 
 Video.create(
 	title: "Ponyo", 
 	description: "During a forbidden excursion to the surface world, a goldfish princess (Yuria Nara) becomes more and more human after befriending a village boy.",
-	small_cover_url: "ponyo.jpg",
-	large_cover_url: "large_ponyo.jpg" )
+	small_cover_url: "/tmp/ponyo.jpg",
+	large_cover_url: "/tmp/large_ponyo.jpg" )
 
 Video.create(
 	title: "Mononoke", 
 	description: "A prince becomes involved in the struggle between a forest princess and the encroachment of mechanization.",
-	small_cover_url: "mononoke.jpg",
-	large_cover_url: "large_mononoke.jpg" )
+	small_cover_url: "/tmp/mononoke.jpg",
+	large_cover_url: "/tmp/large_mononoke.jpg" )
 
 Video.create(
 	title: "Porco Rosso", 
 	description: "In Italy in the 1930s, sky pirates in biplanes terrorize wealthy cruise ships as they sail the Adriatic Sea. The only pilot brave enough to stop the scourge is the mysterious Porco Rosso (Shuichiro Moriyama), a former World War I flying ace who was somehow turned into a pig during the war.",
-	small_cover_url: "rosso.jpg",
-	large_cover_url: "large_rosso.jpg" )
+	small_cover_url: "/tmp/rosso.jpg",
+	large_cover_url: "/tmp/large_rosso.jpg" )
