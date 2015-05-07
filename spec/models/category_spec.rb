05fe611f1c6bfcd4069448f17518aff3922d5ca4
vajradog <vajradog@gmail.com>
2015-05-07 08:59:24 -0400

    installed shoulda matchers gem and implemented it

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,18 +1,6 @@
 require 'rails_helper'
 
 describe Category do 
-	it "saves itself" do 
-		category = Category.new(name: "Drama")
-		category.save
-		expect(Category.first).to eq(category)
-	end
-
-	it "has many videos" do 
-		drama = Category.create(name: "drama")
-		breaking_bad = Video.create(title: "Breaking Bad", description: "animated feature",
-			small_cover_url: "/tmp/spirited_away.jpg", large_cover_url: "/tmp/spirited_away_large.jpg", category: drama)
-		walking_dead = Video.create(title: "Walking Dead", description: "animated feature",
-			small_cover_url: "/tmp/spirited_away.jpg", large_cover_url: "/tmp/spirited_away_large.jpg", category: drama)
-		expect(drama.videos).to eq([breaking_bad, walking_dead])
-	end
+	it { should have_many(:videos)}
+	
 end
