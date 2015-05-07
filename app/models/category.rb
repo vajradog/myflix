e67f09b5a30157927a0060556add7b08ea4cc7bd
vajradog <vajradog@gmail.com>
2015-05-07 16:51:31 -0400

    added recent_videos, added to view files, category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,15 @@
 class Category < ActiveRecord::Base
 	has_many :videos, -> { order("title")}
 	validates :name, presence: true
+
+	def recent_videos	
+		if self.videos.count < 6
+		  videos.all
+		else
+			videos.limit(6).order("created_at DESC")
+		end
+	end
+
+
+
 end
