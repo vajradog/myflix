95f216255cc6a9224ba740f16943ea706254476e
vajradog <vajradog@gmail.com>
2015-05-09 19:51:50 -0400

    refactored video_spec using using 'before'

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,40 +6,34 @@ describe Video do
   it { should validate_presence_of(:description)}
 
   describe "search by title" do 
+    before do 
+      @totoro = Fabricate(:video, title: "totoro")
+      @ponyo = Fabricate(:video, title: "Ponyo", created_at: 1.day.ago)
+    end
+   
+
     it "returns empty array when match not found" do 
-      totoro = Video.create(title:"Totoro", description:"cool animation")
-      ponyo = Video.create(title:"Ponyo", description:"another cool animation")
-      expect(Video.search_by_title("Family")).to eq([])
+      expect(Video.search_by_title("Whatever")).to eq([])
     end
 
     it "returns an array of one video when one match found" do 
-      totoro = Video.create(title:"Totoro", description:"cool animation")
-      ponyo = Video.create(title:"Ponyo", description:"another cool animation")
-      expect(Video.search_by_title("Totoro")).to eq([totoro])
+      expect(Video.search_by_title("Totoro")).to eq([@totoro])
     end
 
     it "returns an array of one video when partial match found" do 
-      totoro = Video.create(title:"Totoro", description:"cool animation")
-      ponyo = Video.create(title:"Ponyo", description:"another cool animation")
-      expect(Video.search_by_title("Tot")).to eq([totoro])
+      expect(Video.search_by_title("Tot")).to eq([@totoro])
     end
 
     it "returns an array of one video when mixed case found" do 
-      totoro = Video.create(title:"Totoro", description:"cool animation")
-      ponyo = Video.create(title:"Ponyo", description:"another cool animation")
-      expect(Video.search_by_title("tOTorO")).to eq([totoro])
+      expect(Video.search_by_title("tOTorO")).to eq([@totoro])
     end
 
     it "returns an array of all matches ordered by created_at when multiple matches found" do 
-      totoro = Video.create(title:"Totoro", description:"cool animation", created_at: 1.day.ago)
-      tot = Video.create(title:"Tot", description:"Childrens animation")
-      ponyo = Video.create(title:"Ponyo", description:"another cool animation")
-      expect(Video.search_by_title("Tot")).to eq([tot, totoro])
+      tot = Fabricate(:video, title: "Tot")
+      expect(Video.search_by_title("Tot")).to eq([tot, @totoro])
     end
 
     it "returns an empty array search term is empty string" do 
-      tot = Video.create(title:"Tot", description:"Childrens animation")
-      ponyo = Video.create(title:"Ponyo", description:"another cool animation")
       expect(Video.search_by_title("")).to eq([])
     end
   end
