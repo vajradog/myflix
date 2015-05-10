af178ffdbd71f539f7167ac4fc7b7a33b09e91ee
vajradog <vajradog@gmail.com>
2015-05-09 20:24:33 -0400

    refactored category_spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,28 +5,29 @@ describe Category do
   it { should validate_presence_of(:name)}
 
   describe "recent videos" do 
+    before do 
+      @drama = Category.create(name: "Drama")
+    end 
+    let(:subject) { @drama.recent_videos }
+
     it "should show only 6 videos if more than 6 videos exists in a category" do 
-      thriller = Category.create(name: "thriller")
       7.times do 
-        video = Video.create(title:"24", description: "Fast thriller", category_id: thriller.id)
+        Fabricate(:video, category_id: @drama.id)
       end
-      expect(thriller.recent_videos.count).to be(6)
+      subject.count.should == (6)
     end
 
     it "should show all videos if less than 6 videos exists in a category" do
-      comedy = Category.create(name: "comedy")
       4.times do 
-        video = Video.create(title: "Family Guy", description: "Funny show", category_id: comedy.id)
+        Fabricate(:video, category_id: @drama.id)
       end
-      expect(comedy.recent_videos.count).to eq(comedy.videos.count)
+      subject.count.should == (@drama.videos.count)
     end
 
     it "should show all videos order by created_at" do 
-      drama = Category.create(name: "comedy")
-      west_wing = Video.create(title: "West Wing", description: "Political Thriller", category_id: drama.id, created_at: 1.day.ago)
-      breaking_bad = Video.create(title: "Breaking Bad", description: "Political Thriller", category_id: drama.id)
-      expect(drama.recent_videos).to eq([breaking_bad, west_wing])
-
+      west_wing = Fabricate(:video, title:"West Wing", category_id: @drama.id, created_at: 1.day.ago)
+      breaking_bad = Fabricate(:video, title: "Breaking Bad", category_id: @drama.id)
+      subject.should == ([breaking_bad, west_wing])
     end
   end
 end
