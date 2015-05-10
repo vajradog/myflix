20141d6addca5d46c4142a3c195534bd37a9403f
vajradog <vajradog@gmail.com>
2015-05-09 20:31:50 -0400

    :should is deprecated so using :expect to refactor

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -14,20 +14,21 @@ describe Category do
       7.times do 
         Fabricate(:video, category_id: @drama.id)
       end
-      subject.count.should == (6)
+      expect(subject.count).to eq(6)
     end
 
     it "should show all videos if less than 6 videos exists in a category" do
       4.times do 
         Fabricate(:video, category_id: @drama.id)
       end
-      subject.count.should == (@drama.videos.count)
+      expect(subject.count).to eq(@drama.videos.count)
     end
 
     it "should show all videos order by created_at" do 
-      west_wing = Fabricate(:video, title:"West Wing", category_id: @drama.id, created_at: 1.day.ago)
+      west_wing = Fabricate(:video, title:"West Wing", category_id: @drama.id, created_at: 2.days.ago)
       breaking_bad = Fabricate(:video, title: "Breaking Bad", category_id: @drama.id)
-      subject.should == ([breaking_bad, west_wing])
+      expect(subject).to eq([breaking_bad, west_wing])
     end
   end
+  
 end
