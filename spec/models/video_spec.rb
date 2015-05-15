51c7ae5d4678498dfc5676941c51dbb64eec197e
vajradog <vajradog@gmail.com>
2015-05-15 12:44:54 -0400

    Week3_PartA

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -8,30 +8,28 @@ describe Video do
   it { should have_many(:queue_items) }
 
   describe ".search_by_title" do 
-    before do 
-      @totoro = Fabricate(:video, title: "totoro")
-      @ponyo = Fabricate(:video, title: "Ponyo", created_at: 1.day.ago)
-    end
+    let(:totoro) { Fabricate(:video, title: "totoro") }
+    let(:ponyo) { Fabricate(:video, title: "Ponyo", created_at: 1.day.ago) }
 
     it "returns empty array when match not found" do 
       expect(Video.search_by_title("Whatever")).to eq([])
     end
 
     it "returns an array of one video when one match found" do 
-      expect(Video.search_by_title("Totoro")).to eq([@totoro])
+      expect(Video.search_by_title("Totoro")).to eq([totoro])
     end
 
     it "returns an array of one video when partial match found" do 
-      expect(Video.search_by_title("Tot")).to eq([@totoro])
+      expect(Video.search_by_title("Tot")).to eq([totoro])
     end
 
     it "returns an array of one video when mixed case found" do 
-      expect(Video.search_by_title("tOTorO")).to eq([@totoro])
+      expect(Video.search_by_title("tOTorO")).to eq([totoro])
     end
 
     it "returns an array of all matches ordered by created_at when multiple matches found" do 
-      tot = Fabricate(:video, title: "Tot")
-      expect(Video.search_by_title("Tot")).to eq([tot, @totoro])
+      tot = Fabricate(:video, title: "Tot", created_at: 1.day.ago)
+      expect(Video.search_by_title("Tot")).to eq([totoro, tot])
     end
 
     it "returns an empty array search term is empty string" do 
